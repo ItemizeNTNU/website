51a96b5676773ba444b4bcd1d2dd70a6723d2bd5
Kristian <kristianint@outlook.com>
2021-04-30 09:42:47 +0200

    update bedrift page

--- src/routes/for-bedrifter.svelte
+++ src/routes/for-bedrifter.svelte
@@ -1,16 +1,45 @@
+<script>
+	import People from '../components/People.svelte';
+
+	const people = [
+		{
+			name: 'Håvard Borgen Myrekrok',
+			position: 'Bedriftskontakt',
+			mail: 'havard.myrekrok@itemize.no',
+		},
+		{
+			name: 'Erik Borgeteien Hansen',
+			position: 'Bedriftskontakt',
+			mail: 'erik.borgeteien.hans@itemize.no',
+		},
+	];
+</script>
+
 <svelte:head>
 	<title>For Bedrifter</title>
 </svelte:head>
 
 <main>
 	<h1>For Bedrifter</h1>
-
-	<p>Er du en bedrift eller annen ekstern part som ønsker å gjennomføre et sammarbeid med oss og våre medlemmer?</p>
-
 	<p>
-		Itemize NTNU er NTNUs offisielle CTF-lag og en åpen interesseorganisasjon for alle ved NTNU med interesse for
-		IT-sikkerhet.
+		Itemize NTNU er NTNU Trondheims CTF-lag og en åpen interesseorganisasjon for alle ved NTNU med interesse for
+		IT-sikkerhet. Vi ønsker å styrke den faglige kompetansen til våre medlemmer. Dette oppnår vi gjennom å holde
+		kurs og CTF-konkurranser, og vi samarbeider gjerne med bedrifter som vil bidra til kompetansehevingen til våre
+		medlemmer. Her er mulighetene mange, og vi er åpne for innspill til format på arrangement.
 	</p>
 
-	<p>Kontakt oss gjerne på e-post <a href="mailto:hack@itemize.no">hack@itemize.no</a>.</p>
+	<h2>Hva vi kan bistå med</h2>
+	<p>
+		Under et arrangement kan vi bistå med digital kommunikasjonskanal, profilering av arrangementet overfor våre
+		medlemmer, og påmelding. Ved behov kan vi også bidra med CTF-oppgaver eller andre praktiske gjøremål tilknyttet
+		arrangementet.
+	</p>
+
+	<h2>Kontakt</h2>
+	<p>
+		Er du en bedrift eller annen ekstern part som ønsker å gjennomføre et samarbeid med oss og våre medlemmer, eller
+		har noe du lurer på? Ta kontakt med oss på <a href="mailto:bedrift@itemize.no">bedrift@itemize.no</a>.
+	</p>
+
+	<People {people} />
 </main>
