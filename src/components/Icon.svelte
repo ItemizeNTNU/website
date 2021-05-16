36a4d7ebceeab6af5c949eec735401ab9a5ae18f
Kristian <kristianint@outlook.com>
2021-05-16 17:46:05 +0200

    User Auth PoC

---
+++ src/components/Icon.svelte
@@ -0,0 +1,10 @@
+<span>
+	<slot />
+</span>
+
+<style>
+	span {
+		display: inline-block;
+		height: 1em;
+	}
+</style>
--- src/server.js
+++ src/server.js
@@ -1,17 +1,41 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import { auth } from 'express-openid-connect';
+import dotenv from 'dotenv';
 
-const { PORT, NODE_ENV } = process.env;
+dotenv.config();
+const { PORT, NODE_ENV, CLIENT_ID, CLIENT_SECRET } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+express()
+	.disable('x-powered-by')
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
-		sapper.middleware()
+		auth({
+			issuerBaseURL: 'https://auth.itemize.no',
+			baseURL: dev ? 'http://localhost:3000' : 'https://itemize.no',
+			clientID: CLIENT_ID,
+			secret: CLIENT_SECRET,
+			idpLogout: false,
+			idTokenSigningAlg: 'HS256',
+			authRequired: false,
+		}),
+		(req, res, next) => { // create req.user object
+			const user = req.oidc?.user;
+			if (user) {
+				const { name, roles, email } = req.oidc?.user;
+				req.user = { name, roles, email };
+			}
+			next();
+		}
 	)
+	.get('/api/user', async (req, res) => {
+		res.send({ "user": req.user });
+	})
+	.use(sapper.middleware())
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
