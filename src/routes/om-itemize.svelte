e08f143c36e18c4b921f4eb6b44a569219bb2f5a
Kristian <kristianint@outlook.com>
2021-05-22 16:34:01 +0200

    fix ctf and event manager email (#12)

--- src/routes/om-itemize.svelte
+++ src/routes/om-itemize.svelte
@@ -1,36 +1,36 @@
 <script>
-	import People from '../components/People.svelte';
+	import People from "../components/People.svelte";
 
 	const board = [
 		{
-			name: 'Aleksander Styrmoe',
-			position: 'Leder',
-			mail: 'leder@itemize.no',
+			name: "Aleksander Styrmoe",
+			position: "Leder",
+			mail: "leder@itemize.no",
 		},
 		{
-			name: 'Sigurd Varhaugvik',
-			position: 'Nestleder',
-			mail: 'nestleder@itemize.no',
+			name: "Sigurd Varhaugvik",
+			position: "Nestleder",
+			mail: "nestleder@itemize.no",
 		},
 		{
 			name: "Kristian in't Veld",
-			position: 'Driftsansvarlig',
-			mail: 'drift@itemize.no',
+			position: "Driftsansvarlig",
+			mail: "drift@itemize.no",
 		},
 		{
-			name: 'Anna Fridtun Aarekol',
-			position: 'Prosjektansvarlig',
-			mail: 'prosjekt@itemize.no',
+			name: "Anna Fridtun Aarekol",
+			position: "Prosjektansvarlig",
+			mail: "prosjekt@itemize.no",
 		},
 		{
-			name: 'Håkon Tjomsland',
-			position: 'CTF- og arrangementsansvarlig',
-			mail: 'arrangement@itemeize.no',
+			name: "Håkon Tjomsland",
+			position: "CTF- og arrangementsansvarlig",
+			mail: "arrangement@itemize.no",
 		},
 		{
-			name: 'Knut Formo Buene',
-			position: 'PR- og rekrutteringsansvarlig',
-			mail: 'pr@itemize.no',
+			name: "Knut Formo Buene",
+			position: "PR- og rekrutteringsansvarlig",
+			mail: "pr@itemize.no",
 		},
 	];
 </script>
@@ -43,36 +43,24 @@
 	<h1>Om Itemize</h1>
 
 	<p>
-		Itemize NTNU er en frivillig interesseorganisasjon for studenter og ansatte ved NTNU Trondheim som liker eller
-		vil lære om informasjonssikkerhet og hacking. I vedtektene til Itemize NTNU står det også at Itemize skal være
-		med på å fremme god sikkerhetskultur i Norge. Dette gjør vi ved å delta på både nasjonale og internasjonale
-		CTFer jevnlig samt å holde både interne og eksterne kurs.
+		Itemize NTNU er en frivillig interesseorganisasjon for studenter og ansatte ved NTNU Trondheim som liker eller vil lære om informasjonssikkerhet og hacking. I vedtektene til
+		Itemize NTNU står det også at Itemize skal være med på å fremme god sikkerhetskultur i Norge. Dette gjør vi ved å delta på både nasjonale og internasjonale CTFer jevnlig samt å
+		holde både interne og eksterne kurs.
 	</p>
 	<p>
-		Driften til Itemize er prosjektbasert, noe som betyr at medlemmer kan velge å engasjere seg i kortvarige
-		prosjekter. Dette sikrer at Itemize utvikler seg videre. Det er selvfølgelig også mulig å være medlem uten å
-		være med på prosjekter, men heller bare være med på CTFer og andre arrangementene man har mulighet til.
+		Driften til Itemize er prosjektbasert, noe som betyr at medlemmer kan velge å engasjere seg i kortvarige prosjekter. Dette sikrer at Itemize utvikler seg videre. Det er
+		selvfølgelig også mulig å være medlem uten å være med på prosjekter, men heller bare være med på CTFer og andre arrangementene man har mulighet til.
 	</p>
+	<p>Itemize NTNU er tilknyttet Institutt for informasjonssikkerhet og kommunikasjonsteknologi og får store deler av støtten til driften derifra.</p>
+	<p>Vi har også en alumniordning for tidligere medlemmer, slik at de kan være med videre på å hjelpe fremtidens sikkerhetsinteresserte.</p>
 	<p>
-		Itemize NTNU er tilknyttet Institutt for informasjonssikkerhet og kommunikasjonsteknologi og får store deler av
-		støtten til driften derifra.
-	</p>
-	<p>
-		Vi har også en alumniordning for tidligere medlemmer, slik at de kan være med videre på å hjelpe fremtidens
-		sikkerhetsinteresserte.
-	</p>
-	<p>
-		For medlemmer bruker vi <a href="https://discord.gg/Et9cCKnyg9" target="_blank" rel="noopener noreferrer"
-			>Discord</a
-		> som hovedkommunikasjonskanal.
+		For medlemmer bruker vi <a href="https://discord.gg/Et9cCKnyg9" target="_blank" rel="noopener noreferrer">Discord</a> som hovedkommunikasjonskanal.
 	</p>
 
 	<h2>Kontakt</h2>
 
 	<p>
-		Vi tar imot innmeldinger, spørsmål og andre henvendelser på mail: <a href="mailto:hack@itemize.no"
-			>hack@itemize.no</a
-		>
+		Vi tar imot innmeldinger, spørsmål og andre henvendelser på mail: <a href="mailto:hack@itemize.no">hack@itemize.no</a>
 	</p>
 	<p>
 		<i>Postadresse:</i><br />
