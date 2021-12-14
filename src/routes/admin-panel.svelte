1418f0ccc133969e3f8eedbc4a83daf1f9562e25
Sigurd Varhaugvik <sigurd@varhaugvik.com>
2021-12-15 00:04:15 +0100

    Added table with user info, with ordering and opening capability'

--- src/routes/admin-panel.svelte
+++ src/routes/admin-panel.svelte
@@ -1,5 +1,6 @@
 <script context="module">
 	import api from '../utils/api';
+	
 
 
 	export async function preload(page, session) {
@@ -9,15 +10,58 @@
 			this.redirect(401, '/')
 		}
 		const query = 'queryString=*'
-		const dbUser = await api.searchUsers(query,{ fetch: this.fetch });
-		return { users: dbUser.json };
+		const dbUsers = await api.searchUsers(query,{ fetch: this.fetch });
+		console.log(dbUsers.json.users)
+		return { users: dbUsers.json.users };
 	}
 </script>
 
 <script>
+	import AdminPanelTable from "../components/AdminPanelTable.svelte";
 	export let users;
-	console.log("ikke sess")
 	console.log(users)
+	console.log(Object.values(users))
+	let selectedCols = ["fullName","discordName", "email", "type", "roles"];
+
+	const COLUMNS = {
+		fullName: {
+			key: "fullName",
+			title: "Navn",
+			value: v => v.fullName,
+			sortable: true,
+		},
+		discordName: {
+			key: "discordName",
+			title: "Discord-navn",
+			value: v => v.discordUsername || "",
+			sortable: true,
+		},
+		email: {
+			key: "email",
+			title: "E-Post",
+			value: v => v.email,
+			sortable: true,
+		},
+		type: {
+			key: "type",
+			title: "Medlemstype",
+			value: v => v.type || "",
+			sortable: true,
+		},
+		roles: {
+			key: "roles",
+			title: "Roller",
+			value: v => v.roles,
+			sortable: true,
+		},
+	};
+
+	$: cols = selectedCols.map(key => COLUMNS[key]);
+
+	function handleExpand(event) {
+		const row = event.detail.row;
+		const operation = row.$expanded ? "open" : "close";
+	}
 </script>
 
 <svelte:head>
@@ -25,5 +69,33 @@
 </svelte:head>
 
 <main>
-
+	<div class="container">
+		<div class="row">
+			<h2>Expand row example 2</h2>
+		
+			<p>
+			  Only 1 row can be expanded at a time<br />
+			  Console logs selection change
+			</p>
+		
+			<AdminPanelTable
+			  columns={cols}
+			  rows={users}
+			  classNameTable="table"
+			  classNameThead="table-info"
+			  showExpandIcon={true}
+			  expandSingle={true}
+			  expandRowKey="fullName"
+			  iconExpand="⌄"
+			  iconExpanded="⌃"
+			  on:clickExpand={handleExpand}
+			>
+			  <div slot="expanded" let:row class="text-center">
+				{row.county}, {row.state}<br />
+				{row.country}
+			  </div>
+			</AdminPanelTable>
+		  </div>
+		</div>
 </main>
+
