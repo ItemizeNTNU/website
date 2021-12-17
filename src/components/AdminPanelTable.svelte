7563350da4d55938df0e08e0be295e1f8512df8a
Sigurd Varhaugvik <sigurd@varhaugvik.com>
2021-12-17 18:38:02 +0100

    Added support for pageination

--- src/components/AdminPanelTable.svelte
+++ src/components/AdminPanelTable.svelte
@@ -29,10 +29,19 @@
 
 	let iconExpand = '⌄';
 	let iconExpanded = '⌃';
-
+	let maxPerPage = 10;
+	let page = 1;
 	let advancedSearch = false;
 	let sortFunction = () => '';
 	let searchValue = '';
+
+	const increasePage = ()=> {
+		page+= page*maxPerPage>=c_rows.length ? 0 : 1;
+	}
+	const decreasePage = ()=>{
+		page-= page==1 ? 0 : 1;
+	}
+
 	// Validation
 	if (!Array.isArray(expanded)) throw "'expanded' needs to be an array";
 
@@ -50,9 +59,11 @@
 	$: colspan = 1 + columns.length;
 
 	const simpleFilter = (rows, searchValue) => {
+		page = 1;
 		return rows.filter((r) => simpleFilterOptions.filter(r, searchValue));
 	};
 	const advancedFilter = (rows, filters) => {
+		page = 1;
 		return rows.filter((r) => advancedFilterOptions.every((e) => e.filter(r, filters[e.name])));
 	};
 	// Filter rows
@@ -135,7 +146,7 @@
 	</thead>
 
 	<tbody>
-		{#each c_rows as row, n}
+		{#each c_rows.slice((page-1)*maxPerPage, page*maxPerPage) as row, n}
 			<tr on:click={(e) => handleClickExpand(e, row)} class="expand pointer">
 				{#each columns as col}
 					<td>{col.value(row)}</td>
@@ -150,8 +161,29 @@
 		{/each}
 	</tbody>
 </table>
+<div class="pageination" style="display:grid;grid-template-columns: 33% 33% 34% ">
+	<p style="text-align:left" on:click={decreasePage}>←</p>
+	<p style="text-align:center">{(page-1)*maxPerPage+(c_rows.length!=0 ? 1 :0)}-{Math.min(page*maxPerPage, c_rows.length)} av {c_rows.length}</p>
+	
+	<p style="text-align:right " on:click={increasePage}>→</p>
+	<span>
+		<p>Resultater per side:</p>
+		<select bind:value={maxPerPage} style="margin-left:10px;width:70px">
+			{#each [5, 10, 15, 20, 40, 60, 100] as num}
+				<option value={num}>{num}</option>
+			{/each}
+		</select>
+	</span>
+</div>
 
 <style>
+	.pageination p, select{
+		margin: 0px;
+		margin-top: 15px;
+	}
+	.pageination span * {
+		display: inline;
+	}
 	.filterOptions p {
 		display: inline;
 		margin: 0px;
